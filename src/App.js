d78776ae48d7bface740111d3d46630682d23d89
YuliaKrimerman <46899367+YuliaKrimerman@users.noreply.github.com>
2019-06-18 12:08:57 -0400

    update changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WineList from './WineList';
+import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchTerm: '',
+			wineData: []	
+	}
+	}
+	searchTermUpdate(term) {
+		this.setState({
+			searchTerm: term
+		})
+	}
+	
+	 handleSubmit(e) {
+        e.preventDefault();
+        this.getWineData(this.state.searchTerm);
+    }
+
+	
+	getWineData(searchTerm) {
+		const url = `http://localhost:8000/wine-api-data/${searchTerm}`
+		console.log(url)
+		fetch(url)
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then(data => {
+			console.log(data)
+				this.setState({
+					wineData: data
+					
+			})
+		})
+			.catch(err => {
+				console.log(err);
+			});
+	}
+
+	
+	
+
+	render() {
+		return (
+			<main className ='App'>
+			<Header
+			/>
+				<div className="query">
+			<img id="glass" src="https://img1.goodfon.com/original/1920x1080/6/e8/wine-glass-wine-wood.jpg">
+			</img>
+			<div className="text-box">
+				<h1>Explore the world of wine</h1>
+				<h2>Do you like Red one or White? Or maybe you're actualy a Rose soul. Type in the wine you love and share your thoughts about it</h2>
+			  <div className="container">
+                <form
+                    className="input_form"
+                    onSubmit={e => this.handleSubmit(e)}>
+                    <div className="search-section top">
+                        <input
+						className="search__input"
+						placeholder="Type in Wine Name"
+                          type="text" 
+                            onChange={e=> this.searchTermUpdate(e.target.value)}
+                        />
+                    </div>
+                        
+                	<div className="buttons-coll">
+					<button type="submit" className="custom-btn btn-4"><span>Search </span></button></div>
+          </form>
+			</div>
+		</div>
+            </div>
+			<WineList
+          		wineDataDisplay={this.state.wineData}
+        />
+		 
+			</main>
+		);
+	}
 }
-
 export default App;
